ad58b75736aabb4f9d8f75f36c5d256ea1325e11
t-yoshimura <t-yoshimura@btm.private>
2020-08-12 20:55:40 +0900

    CRUD処理実施

--- src/main/java/com/example/demo/domain/repository/ItemMapper.java
+++ src/main/java/com/example/demo/domain/repository/ItemMapper.java
@@ -4,11 +4,23 @@ import java.util.List;
 
 import com.example.demo.domain.entity.Item;
 
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface ItemMapper {
     @Select("SELECT * FROM item")
     List<Item> findAll();
+
+    @Insert("INSERT INTO item VALUES(#{id}, #{name}, #{price}, #{url})")
+    public int create(Item item);
+
+    @Update("UPDATE item SET name=#{name}, price=#{price}, url=#{url} WHERE id = #{id}")
+    public int update(Item item);
+
+    @Delete("DELETE FROM item WHERE id = #{id}")
+    public int delete(Item item);
 }
