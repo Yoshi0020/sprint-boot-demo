b9a822d337da6da5616cdc19bd808b03a7ee9a99
t-yoshimura <t-yoshimura@btm.private>
2020-08-14 21:28:44 +0900

    入力チェック処理追加

--- src/main/java/com/example/demo/app/controller/ItemController.java
+++ src/main/java/com/example/demo/app/controller/ItemController.java
@@ -2,12 +2,17 @@ package com.example.demo.app.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
+import com.example.demo.app.form.ItemForm;
 import com.example.demo.domain.entity.Item;
 import com.example.demo.domain.service.ItemService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,12 +36,19 @@ public class ItemController {
 
 	@GetMapping("regist")
 	public String showRegist(Model model) {
+		model.addAttribute("form", new ItemForm());
 		return "item/regist";
 	}
 
 	@PostMapping("regist")
-	public String regist(@ModelAttribute("item") Item item, Model model) {
-		service.save(item);
+	public String regist(@ModelAttribute("form") @Valid ItemForm form, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			
+			model.addAttribute("form", form);
+			return "item/regist";
+		}
+
+		service.save(form.createItem());
 
 		List<Item> itemList = service.findAll();
 
